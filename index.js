cce071552aab9eafca55a1815a8e32b560f824a8
YashChauhan24 <chauhanyash2412@gmail.com>
2024-04-11 11:40:25 +0530

    Node farm done using templating

---
+++ index.js
@@ -0,0 +1,66 @@
+const fs = require('fs');
+const http = require('http');
+const URL = require('url');
+const slugify = require('slugify');
+const replaceTemplate = require('./modules/replaceTemplate');
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const productData = JSON.parse(data);
+const slugs = productData.map((el) => slugify(el.productName, { lower: true }));
+
+const server = http.createServer((req, res) => {
+  const { query, pathname } = URL.parse(req.url, true);
+  // overview page
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(404, { 'Content-type': 'text/html' });
+
+    const cardsHtml = productData.map((el) => replaceTemplate(tempCard, el)).join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(output);
+
+    // product page
+  } else if (pathname === '/product') {
+    res.writeHead(404, { 'Content-type': 'text/html' });
+
+    const product = productData[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+
+    // API
+  } else if (pathname === '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json' });
+    res.end(data);
+
+    // Not found
+  } else {
+    res.writeHead(404, { 'Content-type': 'text/html' });
+    res.end('<h1>Page Not found!</h1>');
+  }
+});
+
+server.listen(8001, '127.0.0.1', () => {
+  console.log('listening to requests on port 8001');
+});
+
+// blocking, syncronous way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+
+//non-blocking, asyncronous way
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+//       console.log(data3);
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) =>
+//         console.log(err)
+//       );
+//     });
+//   });
+// });
+// console.log("Will read file!");
